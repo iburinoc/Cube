a86a27a97c2c9abe258e3552c311b05a6befdbfe
Iburinoc <iburinoc@gmail.com>
2014-01-03 14:17:31 -0500

    Now you can specify a single file to compile :D

--- build.py
+++ build.py
@@ -6,14 +6,18 @@ import sys
 valid = []
 
 o = '-O3 ' if len(sys.argv) >= 2 and sys.argv[1] == '-O' else ''
-
-for root, dirs, filenames in os.walk('./'):
-    for f in filenames:
-        if(f[f.find('.'):] == '.cpp' and (len(f) < 8 or f[f.find('.') - 4: f.find('.')] != 'test')):
-            print f
-            #valid.append(f)
-            os.system('g++ -c -Wall ' + o + '-o bin/' + f + '.o ' + f)
-            print 'g++ -c -Wall ' + o + '-o bin/' + f + '.o ' + f
+offset = 1 if o == '' else 2
+if len(sys.argv) >= offset:
+    os.system('g++ -c -Wall ' + o + '-o bin/' + sys.argv[offset] + '.o ' + sys.argv[offset])
+    print 'g++ -c -Wall ' + o + '-o bin/' + sys.argv[offset] + '.o ' + sys.argv[offset]
+else:
+    for root, dirs, filenames in os.walk('./'):
+        for f in filenames:
+            if(f[f.find('.'):] == '.cpp' and (len(f) < 8 or f[f.find('.') - 4: f.find('.')] != 'test')):
+                print f
+                #valid.append(f)
+                os.system('g++ -c -Wall ' + o + '-o bin/' + f + '.o ' + f)
+                print 'g++ -c -Wall ' + o + '-o bin/' + f + '.o ' + f
 #g++ -c -Wall cube.cpp
 #g++ -c -Wall cubesolver.cpp
 
