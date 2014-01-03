ffc0dfaf6418d03c76b1a53f644ae1916c47a8d5
Iburinoc <iburinoc@gmail.com>
2014-01-03 12:12:01 -0500

    wow im dumb

--- solver.cpp
+++ solver.cpp
@@ -5,30 +5,35 @@ cube* read_cube() {
 }
 
 uint64_t binary_search(std::vector<uint128_t>* v, uint128_t t, uint64_t min, uint64_t max) {
+	uint64_t mid = min + ((max - min) / 2);
+//	std::cout << min << std::endl << mid << std::endl << max << std::endl << std::endl;
+	if((*v)[mid] < t) {
+		return binary_search(v, t, mid + 1, max);
+	} else if((*v)[mid] > t){
+		return binary_search(v, t, min, mid);
+	}
+	return mid;
+}	             
+
+void binary_insert(std::vector<uint128_t>* v, uint128_t t, uint64_t min, uint64_t max) {
 	if(min == max) {
-		return -1;
+		v->insert(v->begin()+min, t);
+		return;
 	}
 	
 	uint64_t mid = min + ((max - min) / 2);
-	std::cout << min << std::endl << mid << std::endl << max << std::endl << std::endl;
-	if((*v)[mid] == t) {
-		return mid;
-	} else if((*v)[mid] < t) {
-		return binary_search(v, t, mid + 1, max);
-	} else {
-		return binary_search(v, t, min, mid);
+	if((*v)[mid] < t) {
+		binary_insert(v, t, mid + 1, max);
+		return;
+	} else if((*v)[mid] > t){
+		binary_insert(v, t, min, mid);
+		return;
 	}
-	std::cout << "shouldnt reach here" << std::endl;
-	std::cout << (uint64_t) t << ";" << mid << ";" << (uint64_t) ((*v)[mid]) << std::endl;
-	return -1;
-}	             
-
-void binary_insert(std::vector<uint128_t>* v, uint128_t i, uint64_t min, uint64_t max) {
-	
+	v->insert(v->begin()+mid, t);
 }
 
 cube* solve_cube(cube* start) {
-	vector<uint128>* visited;
+	std::vector<uint128_t>* visited;
 	return 0;
 }
 
--- solvertest.cpp
+++ solvertest.cpp
@@ -1,5 +1,14 @@
 #include "solver.cpp"
 
+void print_vect(std::vector<uint128_t>* p) {
+	std::cout << "Vector: ";
+	int i = p->size();
+	for(int j = 0; j < i; j++){
+		std::cout << (uint64_t) (*p)[j] << " ";
+	}
+	std::cout << std::endl;
+}
+
 void test_binary_search() {
 	std::vector<uint128_t> v;
 	v.push_back(5);
@@ -9,9 +18,22 @@ void test_binary_search() {
 	v.push_back(100000);
 	v.push_back(100001);
 	
-	std::cout << (binary_search(&v, 100002, 0, 6) == -1);
+	std::cout << binary_search(&v, 100000, 0, 6) << std::endl;
+}
+
+void test_binary_insert() {
+	std::vector<uint128_t> v;
+	uint64_t k = -1;
+	uint64_t size = 0;
+	while(k != 0) {
+		std::cin >> k;
+		binary_insert(&v, k, 0, size);
+		size++;
+		print_vect(&v);
+	}
 }
 
 int main() {
 	test_binary_search();
+	test_binary_insert();
 }
