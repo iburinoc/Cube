a3794b421843566816ad9cd2664e541365575a14
Iburinoc <iburinoc@gmail.com>
2014-06-02 18:56:21 -0400

    major rework begun.  stripped most of cube.c

--- cube.h
+++ cube.h
@@ -2,47 +2,29 @@
 #include <string>
 
 #ifndef RUBIKS_CUBE
-#include "cubesolver.h"
+#include "Cubesolver.h"
 #endif
 
-class cube {
+class Cube {
 public:
-	int** c; // array of faces, which are themselves arrays of ints represeting the colors on each tile
-	
-	std::string hist; //history of transforms applied to this cube
-	
-	cube();
-	
-	~cube();
-	
-	std::vector<cube*>* neighbours();
-	
-	cube* transform_roll_z();
-	
-	cube* transform_roll_x();
-	
-	cube* transform_rot_l();
-	
-	cube* transform_rot_r();
-	
-    cube* swap_etl_etf();
+	int c[6][9];
 
-    cube* swap_vbrt_vrft_vflt();
+	Cube();
 
-    cube* rotate_vftr();
+	~Cube();
 
-    cube* rotate_etr();
+	Cube transform_roll_z();
 
-    cube* move_ebd_to_erf();
+	Cube transform_roll_x();
 
-    cube* move_eld_to_efr();
+	Cube transform_rot_l();
 
-	cube* copy();
-	
+	Cube transform_rot_r();
+
 	bool solved();
-	
+
 	void display();
-	
+
 	__uint128_t serialize();
 };
 
