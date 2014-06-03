18c9a8e4a2d3ea52cc7e356a75bd1b8e45df1480
Andrey Boris Khesin <andrey.khesin@gmail.com>
2014-06-02 23:15:21 -0400

    oops

--- Optimize.java
+++ Optimize.java
@@ -4,7 +4,7 @@ public class Optimize {
 	public static void main(String[] args) throws Exception {
 		new File("simpler.txt").createNewFile();
 		FileOutputStream fos = new FileOutputStream("simpler.txt");
-		final int[][] f = new int[][][] {{{-1, 2, 4, -1, 5, 1}, {5, -1, 0, 2, -1, 3}, {1, 3, -1, 4, 0, -1}}};
+		final int[][] f = new int[][] {{{-1, 2, 4, -1, 5, 1}, {5, -1, 0, 2, -1, 3}, {1, 3, -1, 4, 0, -1}}};
 		for (int l = 2; l <= 6; l++) {
 			int[] m = new int[l];
 			boolean c;
