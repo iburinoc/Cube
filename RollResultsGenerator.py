cabd9f9d28d3bad4c19e1706b6fbcc19e54b71c0
doome1337 <dmitry.paramonov@gmail.com>
2014-06-02 23:16:23 -0400

    Optimization pre-generators made. Replace maxim in RollResultsGenerator.py with 12 for all possible things to optimize. It's in Python because I wanted something that would be easy to use, not something powerful. Runs in O(3^2n) time (I think? Quite possibly less. Very likely less). Anyway, RollResults.txt contains all possible moves with maxim moves, and what they optimize to. Run by using python RollResultsGenerator.py | cat > RollResults.txt. That is all for now.

---
+++ RollResultsGenerator.py
@@ -0,0 +1,191 @@
+def creator(n):
+        if (n!=0):
+                c = creator(n-1)
+                r = []
+                f = []
+                l = []
+                for i in c:
+                        r.append("r"+i)
+                        f.append("f"+i)
+                        l.append("l"+i)
+                return r+f+l
+        else:
+                return [""]
+
+maxim = 9
+allMoves = [[],[],[]]
+for i in range(maxim+1):
+        c = creator(i)
+        for j in c:
+                allMoves[0].append(j) 
+
+#0 = Green
+#1 = Red
+#2 = White
+#3 = Orange
+#4 = Yellow
+#5 = Blue
+
+def transform(string, cube):
+        if(len(string)>1):
+                return transform(string[1:], transform(string[0:1], cube))
+        elif(len(string)==1):
+            if (string == "r"):
+                if (cube[0] == 0 and cube[1] == 1):
+                        return [0,2]
+                if (cube[0] == 0 and cube[1] == 2):
+                        return [0,3]
+                if (cube[0] == 0 and cube[1] == 3):
+                        return [0,4]
+                if (cube[0] == 0 and cube[1] == 4):
+                        return [0,1]
+                if (cube[0] == 1 and cube[1] == 0):
+                        return [1,4]
+                if (cube[0] == 1 and cube[1] == 2):
+                        return [1,0]
+                if (cube[0] == 1 and cube[1] == 4):
+                        return [1,5]
+                if (cube[0] == 1 and cube[1] == 5):
+                        return [1,2]
+                if (cube[0] == 2 and cube[1] == 0):
+                        return [2,1]
+                if (cube[0] == 2 and cube[1] == 1):
+                        return [2,5]
+                if (cube[0] == 2 and cube[1] == 3):
+                        return [2,0]
+                if (cube[0] == 2 and cube[1] == 5):
+                        return [2,3]
+                if (cube[0] == 3 and cube[1] == 0):
+                        return [3,2]
+                if (cube[0] == 3 and cube[1] == 2):
+                        return [3,5]
+                if (cube[0] == 3 and cube[1] == 4):
+                        return [3,0]
+                if (cube[0] == 3 and cube[1] == 5):
+                        return [3,4]
+                if (cube[0] == 4 and cube[1] == 0):
+                        return [4,3]
+                if (cube[0] == 4 and cube[1] == 1):
+                        return [4,0]
+                if (cube[0] == 4 and cube[1] == 3):
+                        return [4,5]
+                if (cube[0] == 4 and cube[1] == 5):
+                        return [4,1]
+                if (cube[0] == 5 and cube[1] == 1):
+                        return [5,4]
+                if (cube[0] == 5 and cube[1] == 2):
+                        return [5,1]
+                if (cube[0] == 5 and cube[1] == 3):
+                        return [5,2]
+                if (cube[0] == 5 and cube[1] == 4):
+                        return [5,3]
+            elif (string == "f"):
+                if (cube[0] == 0 and cube[1] == 1):
+                        return [3,0]
+                if (cube[0] == 0 and cube[1] == 2):
+                        return [4,0]
+                if (cube[0] == 0 and cube[1] == 3):
+                        return [1,0]
+                if (cube[0] == 0 and cube[1] == 4):
+                        return [2,0]
+                if (cube[0] == 1 and cube[1] == 0):
+                        return [5,1]
+                if (cube[0] == 1 and cube[1] == 2):
+                        return [4,1]
+                if (cube[0] == 1 and cube[1] == 4):
+                        return [2,1]
+                if (cube[0] == 1 and cube[1] == 5):
+                        return [0,1]
+                if (cube[0] == 2 and cube[1] == 0):
+                        return [5,2]
+                if (cube[0] == 2 and cube[1] == 1):
+                        return [3,2]
+                if (cube[0] == 2 and cube[1] == 3):
+                        return [1,2]
+                if (cube[0] == 2 and cube[1] == 5):
+                        return [0,2]
+                if (cube[0] == 3 and cube[1] == 0):
+                        return [5,3]
+                if (cube[0] == 3 and cube[1] == 2):
+                        return [4,3]
+                if (cube[0] == 3 and cube[1] == 4):
+                        return [2,3]
+                if (cube[0] == 3 and cube[1] == 5):
+                        return [0,3]
+                if (cube[0] == 4 and cube[1] == 0):
+                        return [5,4]
+                if (cube[0] == 4 and cube[1] == 1):
+                        return [1,4]
+                if (cube[0] == 4 and cube[1] == 3):
+                        return [1,4]
+                if (cube[0] == 4 and cube[1] == 5):
+                        return [0,4]
+                if (cube[0] == 5 and cube[1] == 1):
+                        return [3,5]
+                if (cube[0] == 5 and cube[1] == 2):
+                        return [4,5]
+                if (cube[0] == 5 and cube[1] == 3):
+                        return [1,5]
+                if (cube[0] == 5 and cube[1] == 4):
+                        return [2,5]
+            else:
+                if (cube[0] == 0 and cube[1] == 1):
+                        return [0,4]
+                if (cube[0] == 0 and cube[1] == 2):
+                        return [0,1]
+                if (cube[0] == 0 and cube[1] == 3):
+                        return [0,2]
+                if (cube[0] == 0 and cube[1] == 4):
+                        return [0,3]
+                if (cube[0] == 1 and cube[1] == 0):
+                        return [1,2]
+                if (cube[0] == 1 and cube[1] == 2):
+                        return [1,5]
+                if (cube[0] == 1 and cube[1] == 4):
+                        return [1,0]
+                if (cube[0] == 1 and cube[1] == 5):
+                        return [1,4]
+                if (cube[0] == 2 and cube[1] == 0):
+                        return [2,3]
+                if (cube[0] == 2 and cube[1] == 1):
+                        return [2,0]
+                if (cube[0] == 2 and cube[1] == 3):
+                        return [2,5]
+                if (cube[0] == 2 and cube[1] == 5):
+                        return [2,1]
+                if (cube[0] == 3 and cube[1] == 0):
+                        return [3,4]
+                if (cube[0] == 3 and cube[1] == 2):
+                        return [3,0]
+                if (cube[0] == 3 and cube[1] == 4):
+                        return [3,5]
+                if (cube[0] == 3 and cube[1] == 5):
+                        return [3,2]
+                if (cube[0] == 4 and cube[1] == 0):
+                        return [4,1]
+                if (cube[0] == 4 and cube[1] == 1):
+                        return [4,5]
+                if (cube[0] == 4 and cube[1] == 3):
+                        return [4,0]
+                if (cube[0] == 4 and cube[1] == 5):
+                        return [4,3]
+                if (cube[0] == 5 and cube[1] == 1):
+                        return [5,2]
+                if (cube[0] == 5 and cube[1] == 2):
+                        return [5,3]
+                if (cube[0] == 5 and cube[1] == 3):
+                        return [5,4]
+                if (cube[0] == 5 and cube[1] == 4):
+                        return [5,1]
+        else:
+                return cube
+
+for i in range(len(allMoves[0])):
+        allMoves[1].append(transform(allMoves[0][i],[0,1]))
+        for j in range(i+1):
+                if(allMoves[1][j] == allMoves[1][i]):
+                        allMoves[2].append(allMoves[0][j])
+                        break
+
+for i in range(len(allMoves[0])):
+        print allMoves[0][i]+"->"+allMoves[2][i]
