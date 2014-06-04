fd68e594bd76b0e05d950c23450605903f3ca29c
Iburinoc <iburinoc@gmail.com>
2014-06-04 09:01:17 -0400

    andrey yore codes maek me sad face :c sok young padawan maybe some day yoo shall learn da cpps

--- solution.cpp
+++ solution.cpp
@@ -1,11 +1,14 @@
-string solution(Cube c) {
-	string a = "";
-	final int T = c.c[1][4], B = c.c[0][4], int p[][] = {{c.c[2][7], c.c[5][7], c.c[3][7], c.c[4][7]}, {c.c[2][1], c.c[5][1], c.c[3][1], c.c[4][1]}, {c.c[1][7], c.c[5][5], c.c[3][3], c.c[0][1]}, {c.c[1][5], c.c[2][5], c.c[4][3], c.c[0][5]}, {c.c[1][1], c.c[3][5], c.c[5][3], c.c[0][7]}, {c.c[1][3], c.c[4][5], c.c[2][3], c.c[0][3]}};
+#include "solution.h"
+
+std::string solution(Cube c) {
+	std::string a = "";
+	const int T = c.c[1][4], B = c.c[0][4];
+	const int p[][] = {{c.c[2][7], c.c[5][7], c.c[3][7], c.c[4][7]}, {c.c[2][1], c.c[5][1], c.c[3][1], c.c[4][1]}, {c.c[1][7], c.c[5][5], c.c[3][3], c.c[0][1]}, {c.c[1][5], c.c[2][5], c.c[4][3], c.c[0][5]}, {c.c[1][1], c.c[3][5], c.c[5][3], c.c[0][7]}, {c.c[1][3], c.c[4][5], c.c[2][3], c.c[0][3]}};
 	l:
 	for (int t = 2; t < 6; t++) {	
 		for (int i = 0; i < 6; i++) {
 			for (int j = 1; j < 9; j += 2) {
-				if (c.c[i][j] == c.c[1][4] && p[face][index / 2]; == T) {
+				if (c.c[i][j] == c.c[1][4] && p[face][index / 2] == T) {
 					if (i == 1) {
 						if ((j == 1 && c.c[4][4] == T) || (j == 3 && c.c[5][4] == T) || (j == 5 && c.c[3][4] == T) || (j == 7 && c.c[2][4] == T)) {
 							continue l;
@@ -152,3 +155,4 @@ string solution(Cube c) {
 		}
 	}
 }
+
---
+++ solution.h
@@ -0,0 +1,11 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+#include <string>
+
+#include "cube.h"
+
+std::string solution(Cube c);
+
+#endif
+
