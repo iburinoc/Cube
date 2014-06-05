600f055e3a043cb4862932730ca69c3b02954bd3
Iburinoc <iburinoc@gmail.com>
2014-06-05 09:19:21 -0400

    werks :D

--- move_assembler_test.cpp
+++ move_assembler_test.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 #include "cube.h"
 
 int main() {
-	std::string hl = "DUlFR";
-	puts(move_assembler(hl).c_str());
+	std::string moves = "DdUuFfRrBbLl";
+	std::string ops = "";
+	srand(time(NULL));
+	for(int i = 0; i < 5; i++) {
+		int n = rand() % 12;
+		ops += moves[n];
+	}
+	puts(ops.c_str());
+	puts(move_assembler(ops).c_str());
 }
 
