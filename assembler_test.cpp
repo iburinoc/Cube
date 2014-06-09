22a22be09fa858a76540ea6a16a648e829e4f2ef
Iburinoc <iburinoc@gmail.com>
2014-06-09 17:39:37 -0400

    Removed comments there.  ready for master merge.

--- assembler_test.cpp
+++ assembler_test.cpp
@@ -6,28 +6,6 @@
 #include "cube.h"
 #include "assembler.h"
 
-/*
-int main() {
-	std::string moves = "DdUuFfRrBbLl";
-	std::string ops = "";
-	srand(time(NULL));
-	for(int i = 0; i < 5; i++) {
-		int n = rand() % 12;
-		ops += moves[n];
-	}
-	puts(ops.c_str());
-	puts(move_assembler(ops).c_str());
-}
-
-int main() {
-	puts(remove_fours("dUUddddUUllll").c_str());
-	puts(remove_undos_hl("dDUUu").c_str());
-	puts(assemble("RUr").c_str());
-	Trie t = construct_trie("RollResults.txt");
-	puts(t.match("lllfff").result().c_str());
-}
-*/
-
 int main() {
 	std::function<void(Cube&)> hops[128];
 	hops[(int)'D'] = &Cube::D;
