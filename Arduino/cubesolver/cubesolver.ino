d772600f6143cfcdddf25d91dd234a1fe23e20ab
Iburinoc <iburinoc@gmail.com>
2014-06-13 19:39:18 -0400

    Cubesolver pretty close to gud now

--- Arduino/cubesolver/cubesolver.ino
+++ Arduino/cubesolver/cubesolver.ino
@@ -1,20 +1,23 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 
-/*
-const int armPower = ;
-const int basePower = ;
-const int flipperPower = ;
-const int armExtendMovementDelay = ;
-const int armRetractMovementDelay = ;
-const int baseRightRotationDelay = ;
-const int baseLeftRotationDelay = ;
-const int flipperUpRotationDelay = ;
-const int flipperDownRotationDelay = ;
-*/
-const int armMotorNumber = 1;
+const int armPower = 100;
+const int basePower = 100;
+const int flipperPower = 255;
+const int armExtendMovementDelay = 1600;
+const int armRetractMovementDelay = 1600;
+const int baseRightRotationDelay = 230;
+const int baseLeftRotationDelay = 230;
+const int flipperUpRotationDelay = 5600;
+const int flipperDownRotationDelay = 5400;
+
+/*const int armMotorNumber = 1;
 const int baseMotorNumber = 3;
 const int flipperMotorNumber = 4;
+*/
+#define ARM_MOTOR_NUMBER 1
+#define BASE_MOTOR_NUMBER 3
+#define FLIPPER_MOTOR_NUMBER 4
 const int moveSplitDelay = 500;
 const boolean EXTEND = true;
 const boolean RETRACT = false;
@@ -23,79 +26,93 @@ const boolean LEFT = false;
 const boolean UP = true;
 const boolean DOWN = false;
 
-Adafruit_MotorShield AFMS() = Adafruit_ModorShield();
-Adafruit_DCMotor *arm = AFMS.getMotor(armMotorNumber);
-Adafruit_DCMotor *base = AFMS.getMotor(baseMotorNumber);
-Adafruit_DCMotor *flipper = AFMS.getMotor(flipperMotorNumber);
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *armm = AFMS.getMotor(ARM_MOTOR_NUMBER);
+Adafruit_DCMotor *basem = AFMS.getMotor(BASE_MOTOR_NUMBER);
+Adafruit_DCMotor *flipperm = AFMS.getMotor(FLIPPER_MOTOR_NUMBER);
 
 boolean armExtended = false;
 
 void arm(boolean extend) {
-	arm->run(extend ? FORWARD : BACKWARD);
+	armm->run(!extend ? FORWARD : BACKWARD);
 	delay(extend ? armExtendMovementDelay : armRetractMovementDelay);
-	arm->run(RELEASE);
+	armm->run(RELEASE);
 	delay(moveSplitDelay);
 	armExtended = extend;
 }
 
 void base(boolean right) {
-	base->run(right ? FORWARD : BACKWARD);
-	delay(right ? baseRightRotationDelay : baseLeftRotationDelay);
-	base->run(RELEASE);
+	basem->run(right ? FORWARD : BACKWARD);
+	delay((right ? baseRightRotationDelay : baseLeftRotationDelay) * (armExtended ? 2 : 1));
+	basem->run(RELEASE);
 	delay(moveSplitDelay);
 }
 
 void flip(boolean up) {
-	flipper->run(up ? FORWARD : BACKWARD);
+	flipperm->run(up ? FORWARD : BACKWARD);
 	delay(up ? flipperUpRotationDelay : flipperDownRotationDelay);
-	flipper->run(RELEASE);
+	flipperm->run(RELEASE);
 	delay(moveSplitDelay);
 }
 
 void setup() {
 	Serial.begin(9600);
-	arm->setSpeed(armPower);
-	base->setSpeed(basePower);
-	flipper->setSpeed(flipperPower);
-	arm(RETRACT);
+	Serial.println("hello");
+	
+	AFMS.begin();
+	
+	armm->setSpeed(armPower);
+	basem->setSpeed(basePower);
+	flipperm->setSpeed(flipperPower);
+	armm->run(RELEASE);
+	basem->run(RELEASE);
+	flipperm->run(RELEASE);
+	Serial.println("start");
+	delay(1000);
+	Serial.println("stop");
 }
 
 
 void loop() {
-	char instruction = Serial.read()[0];
-	switch (instruction) {
-		case 'r':
-			if (armExtended) {
-				arm(RETRACT);
-			}
-			base(RIGHT);
-			break;
-		case 'l':
-			if (armExtended) {
-				arm(RETRACT);
-			}
-			base(LEFT);
-			break;
-		case 'f':
-			if (armExtended) {
-				arm(RETRACT);
-			}
-			flip(UP);
-			flip(DOWN);
-			arm(EXTEND);
-			break;
-		case 'c':
-			if (!armExtended) {
+	int instruction = Serial.read();
+	if(instruction != -1) {
+		Serial.write(instruction);
+		switch (instruction) {
+			case 'r':
+				if (armExtended) {
+					arm(RETRACT);
+				}
+				base(RIGHT);
+				break;
+			case 'l':
+				if (armExtended) {
+					arm(RETRACT);
+				}
+				base(LEFT);
+				break;
+			case 'f':
+				if (armExtended) {
+					arm(RETRACT);
+				}
+				flip(UP);
+				flip(DOWN);
 				arm(EXTEND);
-			}
-			base(RIGHT);
-			break;
-		case 'w':
-			if (!armExtended) {
-				arm(EXTEND);
-			}
-			base(LEFT);
-			break;
+				arm(RETRACT);
+				break;
+			case 'c':
+				if (!armExtended) {
+					arm(EXTEND);
+				}
+				base(RIGHT);
+				break;
+			case 'w':
+				if (!armExtended) {
+					arm(EXTEND);
+				}
+				base(LEFT);
+				break;
+		}
 	}
+	delay(10);
 }
 
