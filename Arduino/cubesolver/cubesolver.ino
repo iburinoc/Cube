2b4924912719815dd32877e119191040324e80b2
Andrey Boris Khesin <andrey.khesin@gmail.com>
2014-06-12 22:17:12 -0400

    commit to pull

--- Arduino/cubesolver/cubesolver.ino
+++ Arduino/cubesolver/cubesolver.ino
@@ -1,5 +1,36 @@
+/*
+const int aD = ;
+const int aB = ;
+const int aP = ;
+const int bD = ;
+const int bB = ;
+const int bP = ;
+const int fD = ;
+const int fB = ;
+const int fP = ;
+const int t = ;
+const int a = ;
+const int f = ;
+*/
+
+void arm(boolean e) {
+    digitalWrite(aD, e ? HIGH : LOW);
+}
+
 void setup() {
     Serial.begin(9600);
+    pinMode(aD, OUTPUT);
+    pinMode(aB, OUTPUT);
+    pinMode(bD, OUTPUT);
+    pinMode(bB, OUTPUT);
+    pinMode(fD, OUTPUT);
+    pinMode(fB, OUTPUT);
+    digitalWrite(aB, HIGH);
+    digitalWrite(bB, HIGH);
+    digitalWrite(fB, HIGH);
+    analogWrite(aP, 50);
+    analogWrite(bP, 50);
+    analogWrite(fP, 255);
 }
 
 boolean a = false;
@@ -45,8 +76,7 @@ void loop() {
             }
             a = true;
             //command for moving the motor the other way
-            break;    
-           
+            break;
   }
 }
 
